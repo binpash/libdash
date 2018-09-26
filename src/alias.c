8b6da2a3e589246e0b40c32f6e59b2e5bc5210e3
Michael Greenberg <michael.greenberg@pomona.edu>
2018-09-26 09:36:07 -0400

    expose hooks

--- src/alias.c
+++ src/alias.c
@@ -48,11 +48,9 @@
 
 struct alias *atab[ATABSIZE];
 
-STATIC void setalias(const char *, const char *);
 STATIC struct alias *freealias(struct alias *);
 STATIC struct alias **__lookupalias(const char *);
 
-STATIC
 void
 setalias(const char *name, const char *val)
 {
@@ -187,7 +185,7 @@ unaliascmd(int argc, char **argv)
 	return (i);
 }
 
-STATIC struct alias *
+struct alias *
 freealias(struct alias *ap) {
 	struct alias *next;
 
