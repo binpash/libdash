7c1dfcdba6bb4350fa8fc52e336b856b6caa24ca
Michael Greenberg <michael.greenberg@pomona.edu>
2018-09-27 14:27:55 -0400

    correctly building in both docker and my computer

--- ocaml/round_trip.sh
+++ ocaml/round_trip.sh
@@ -9,7 +9,14 @@ p=$1
 tgt=$2
 
 orig=$(${p} ${tgt})
+if [ "$?" -ne 0 ];
+then echo ${tgt} FAILED, couldn\'t run; exit 2
+fi
+
 rt=$(${p} ${tgt} | ${p})
+if [ "$?" -ne 0 ];
+then echo ${tgt} FAILED round trip, couldn\'t run; exit 3
+fi
 
 if [ "${orig}" = "${rt}" ];
 then echo ${tgt} OK; exit 0
