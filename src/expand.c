c4f4ee8ecf85834811c252fc1df3892863572bbd
Nikolai Merinov <n.merinov@inango-systems.com>
2019-04-29 19:13:37 +0500

    expand: Fix trailing newlines processing in backquote expanding
    According to POSIX.1-2008 we should remove newlines only at the end of
    the substitution. Newlines-only substitions causes dash to remove
    newlines before beggining of the substitution. The following code:
    
        cat <<END
        1
        $(echo "")
        2
        END
    
    prints "1<newline>2" instead of expected "1<newline><newline>2".
    
    This patch fixes trailing newlines processing in backquote expanding.
    
    Signed-off-by: Nikolai Merinov <n.merinov@inango-systems.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- src/expand.c
+++ src/expand.c
@@ -525,7 +525,7 @@ read:
 
 	/* Eat all trailing newlines */
 	dest = expdest;
-	for (; dest > (char *)stackblock() && dest[-1] == '\n';)
+	for (; dest > ((char *)stackblock() + startloc) && dest[-1] == '\n';)
 		STUNPUTC(dest);
 	expdest = dest;
 
