422df1687fb91deeb62f4c978e7eab783b3bae36
Michael Greenberg <michael.greenberg@pomona.edu>
2020-02-17 13:20:59 -0800

    pushing

--- ocaml/mk_meta.sh
+++ ocaml/mk_meta.sh
@@ -6,5 +6,5 @@ requires = "ctypes,ctypes.foreign"
 version = "0.1"
 archive(native) = "dash.cmxa"
 archive(byte) = "dash.cma"
-linkopts="-ccopt -L$(opam var stublibs) -cclib -ldash"
+inkopts="-ccopt -L$(ocamlfind query libdash) -cclib -Wl,-rpath=$(ocamlfind query libdash) -cclib -ldash"
 EOF
