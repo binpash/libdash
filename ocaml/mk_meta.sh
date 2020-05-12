c51144972488e4037ec3334a0cbe44cf3a7942e8
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-12 16:52:11 -0700

    just put more options in there

--- ocaml/mk_meta.sh
+++ ocaml/mk_meta.sh
@@ -8,7 +8,7 @@ requires = "ctypes,ctypes.foreign"
 version = "0.1"
 archive(native) = "dash.cmxa"
 archive(byte) = "dash.cma"
-linkopts(native) = "-cclib -ldash"
+linkopts(native) = "-linkpkg -ccopt -L$(opam var lib) -ccopt -Wl,-rpath -ccopt -Wl,$(opam var lib) -cclib -ldash"
 linkopts(byte) = "-dllpath $(opam var lib)/libdash"
 EOF
 
