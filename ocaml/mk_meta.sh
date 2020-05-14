a51ce827b2c28c0428c28c5dc2e4f445e46552df
Michael Greenberg <michael.greenberg@pomona.edu>
2020-05-13 20:19:42 -0700

    debug info on META, trying to clarify rpath... huh.

--- ocaml/mk_meta.sh
+++ ocaml/mk_meta.sh
@@ -8,7 +8,7 @@ requires = "ctypes,ctypes.foreign,str"
 version = "0.1"
 archive(native) = "dash.cmxa"
 archive(byte) = "dash.cma"
-linkopts(native) = "-ccopt -L$(opam var lib) -ccopt -Wl,-rpath -ccopt -Wl,$(opam var lib) -cclib -ldash"
-linkopts(byte) = "-dllpath $(opam var lib)/libdash"
+linkopts(native) = "-ccopt -L$(opam var libdash:lib) -ccopt -Wl,-rpath -ccopt -Wl,$(opam var libdash:lib) -cclib -ldash"
+linkopts(byte) = "-dllpath $(opam var libdash:lib)/libdash"
 EOF
 
