0663687424b502132ae7fc6a3835d5253dd65469
Michael Greenberg <michael.greenberg@stevens.edu>
2022-08-18 09:23:17 -0400

    fix version.sh

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION=$(grep -e '^version =' pyproject.toml | cut -d'=' -f2 | tr -d ' "
 
 PYTHON_VERSION2=$(grep -e 'version=' setup.py | cut -d'=' -f2 | tr -d "',")
 
-[ "$OPAM_VERSION" = "$PYTHON_VERSION" ] && [ "$PYTHON_VERSION" = "$PYTHON_VERSION2" ] && echo "$OPAM_VERSION" && exit 0
+[ "$PYTHON_VERSION" = "$PYTHON_VERSION2" ] && echo "$PYTHON_VERSION" && exit 0
 
 echo "Version numbers don't match!"
 echo "  Python is '$PYTHON_VERSION' in pyproject.toml"
