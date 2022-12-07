d5d616126ccc309d441fa82bb6a5bbf3377bd0e9
Herbert Xu <herbert@gondor.apana.org.au>
2022-12-07 16:55:07 +0800

    eval: Test evalskip before flipping status for NNOT
    On Tue, Dec 06, 2022 at 10:15:03AM +0000, Harald van Dijk wrote:
    >
    > There is a long-standing bug that may or may not be harder to fix if this
    > patch goes in, depending on how you want to fix it. Here's a script that
    > already fails on current dash.
    >
    >   f() {
    >     if ! return 0
    >     then :
    >     fi
    >   }
    >   f
    >
    > This should return 0, and does return 0 in bash and ksh (and almost all
    > shells), but returns 1 in dash.
    >
    > There are a few possible ways of fixing it. Some of them rely on continuing
    > to conditionally set exitstatus.
    
    This can be fixed simply by testing evalskip prior to flipping the
    status.
    
    Reported-by: Harald van Dijk <harald@gigawatt.nl>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- src/eval.c
+++ src/eval.c
@@ -238,7 +238,9 @@ evaltree(union node *n, int flags)
 		break;
 #endif
 	case NNOT:
-		status = !evaltree(n->nnot.com, EV_TESTED);
+		status = evaltree(n->nnot.com, EV_TESTED);
+		if (!evalskip)
+			status = !status;
 		break;
 	case NREDIR:
 		errlinno = lineno = n->nredir.linno;
